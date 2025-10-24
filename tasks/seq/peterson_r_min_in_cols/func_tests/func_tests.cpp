8a3a28989489da3dc5cba8b6fb78cb59c5a2f29f
RomaPeterson <144802312+RomaPeterson@users.noreply.github.com>
2025-10-24 20:03:55 +0300

    fix: add test files and remove accidental files

---
+++ tasks/seq/peterson_r_min_in_cols/func_tests/func_tests.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include "peterson_r_min_in_cols_seq.h"
+
+TEST(peterson_r_min_in_cols_seq, test_basic_functionality) {
+    // Basic test will be implemented
+}
---
+++ tasks/seq/peterson_r_min_in_cols/perf_tests/perf_tests.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+
+TEST(peterson_r_min_in_cols_seq, test_pipeline_run) {
+    // Performance test will be implemented
+}
+
+TEST(peterson_r_min_in_cols_seq, test_task_run) {
+    // Performance test will be implemented
+}
+
+cat > perf_tests/perf_tests.cpp << 'EOF'
+#include <gtest/gtest.h>
+
+TEST(peterson_r_min_in_cols_seq, test_pipeline_run) {
+    // Performance test will be implemented
+}
+
+TEST(peterson_r_min_in_cols_seq, test_task_run) {
+    // Performance test will be implemented
+}
