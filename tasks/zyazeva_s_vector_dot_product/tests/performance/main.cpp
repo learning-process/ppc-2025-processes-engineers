29cc9f377bd9eec21c8fee77545f22859b90f777
Sveta-ls <slana6570@gmail.com>
2025-10-25 17:52:45 +0000

    tests names fixed

--- tasks/zyazeva_s_vector_dot_product/tests/performance/main.cpp
+++ tasks/zyazeva_s_vector_dot_product/tests/performance/main.cpp
@@ -7,7 +7,7 @@
 
 namespace zyazeva_s_vector_dot_product {
 
-class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
+class ZyazevaSElemVecDotProdPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
   const int kCount_ = 100;
   InType input_data_{};
 
@@ -24,7 +24,7 @@ class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, O
   }
 };
 
-TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+TEST_P(ZyazevaSElemVecDotProdPerfTests, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
@@ -33,8 +33,8 @@ const auto kAllPerfTasks =
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+const auto kPerfTestName = ZyazevaSElemVecDotProdPerfTests::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, ZyazevaSElemVecDotProdPerfTests, kGtestValues, kPerfTestName);
 
 }  // namespace zyazeva_s_vector_dot_product
